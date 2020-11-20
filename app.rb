8c48e3c5e6d1be12e694d9faec8786b16d47ca7c
Chris Bellairs <c.m.bellairs@gmail.com>
2020-11-20 15:04:53 +0000

    Tried a different way of passing the information

--- app.rb
+++ app.rb
@@ -8,11 +8,12 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
-csv_file   = File.join(__dir__, 'lib/recipes.csv')
-cookbook   = Cookbook.new(csv_file)
+csv_file = File.join(__dir__, 'lib/recipes.csv')
+cookbook = Cookbook.new(csv_file)
+
 
 get '/' do
-  cookbook.all
+  @recipes = cookbook.all
   erb :index
 end
 
