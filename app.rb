57249d42ce187a12deb06d372f7b6a5f8fa00e17
Chris Bellairs <c.m.bellairs@gmail.com>
2020-11-20 17:04:26 +0000

    Basic challenge completed

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
 require_relative 'lib/cookbook'
+require_relative 'lib/recipe'
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
@@ -11,9 +12,25 @@ end
 csv_file = File.join(__dir__, 'lib/recipes.csv')
 cookbook = Cookbook.new(csv_file)
 
-
 get '/' do
   @recipes = cookbook.all
-  erb :index
+  erb :layout do
+    erb :index
+  end
 end
 
+get '/new' do
+  erb :layout do
+    erb :new
+  end
+end
+
+post '/recipes' do
+  cookbook.add_recipe(Recipe.new(params[:name], params[:description], params[:rating], params[:prep_time]))
+  redirect '/'
+end
+
+get '/delete/:index' do
+  cookbook.remove_recipe(params[:index].to_i)
+  redirect '/'
+end
--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -14,24 +14,20 @@ class Cookbook
 
   def add_recipe(recipe)
     @recipes << recipe
-    CSV.open(@csv_filepath, "wb") do |csv|
-      @recipes.each do |instance|
-        csv << [instance.name, instance.description, instance.rating, instance.prep_time, instance.done]
-      end
-    end
+    save_csv
   end
 
   def remove_recipe(recipe_index)
     @recipes.delete_at(recipe_index)
-    CSV.open(@csv_filepath, "wb") do |csv|
-      @recipes.each do |recipe|
-        csv << [recipe.name, recipe.description, instance.rating, instance.prep_time, instance.done]
-      end
-    end
+    save_csv
   end
 
   def mark_as_done(recipe_index)
     @recipes[recipe_index].mark_as_done
+    save_csv
+  end
+
+  def save_csv
     CSV.open(@csv_filepath, "wb") do |csv|
       @recipes.each do |instance|
         csv << [instance.name, instance.description, instance.rating, instance.prep_time, instance.done]
